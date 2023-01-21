78d4fd78ad553c27f1c5d8f5da0d30f20f860a84
serhiy-v <sergozhulistuj@gmail.com>
2023-01-21 17:26:06 +0200

    Update jenkinsfile

--- testscript.sh
+++ testscript.sh
@@ -7,6 +7,10 @@ sudo apt install apache2 php libapache2-mod-php -y
 
 sudo mkdir -p /var/www/html/
 
+sudo chmod +w /var/www/html/index.html
+
+ls -la /var/www/html/
+
 sudo echo "<h1>Created with  Jenkins by Serhii Valchuk<h1>" > /var/www/html/index.html
 
 sudo service apache2 restart
