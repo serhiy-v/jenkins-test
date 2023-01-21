0aab5b69c632880f8a15edf9b29ba4f09439bae1
serhiy-v <sergozhulistuj@gmail.com>
2023-01-21 18:31:29 +0200

    Edited script

--- testscript.sh
+++ testscript.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
 
-sudo apt update -y
+install_apache() {
+    sudo apt update -y
 
-sudo apt install apache2 php libapache2-mod-php -y
+    sudo apt install apache2 php libapache2-mod-php -y
 
-sudo mkdir -p /var/www/html/
+    sudo mkdir -p /var/www/html/
 
-sudo chown -R jenkins:jenkins /var/www
+    sudo chown -R jenkins:jenkins /var/www
 
-ls -la /var/www/html/
+    ls -la /var/www/html/
 
-sudo echo "<h1>Created with  Jenkins by Serhii Valchuk<h1>" > /var/www/html/index.html
+    sudo echo "<h1>Created with  Jenkins by Serhii Valchuk<h1>" > /var/www/html/index.html
 
-sudo service apache2 restart
+    sudo service apache2 restart
+}
 
-#TERRAFORM_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |  grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
-
-#wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
-
-#sudo apt install unzip -y
-
-#unzip terraform_${TERRAFORM_VER}_linux_amd64.zip -y
-
-#sudo mv terraform /usr/local/bin/
-
-#terraform version
+if pgrep -x "apache2" >/dev/null
+then
+    echo "Server already installed"
+else
+    install_apache
+fi
