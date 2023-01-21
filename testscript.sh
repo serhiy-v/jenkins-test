1919e7379b156dc775b623d5b53ffec942f043f4
serhiy-v <sergozhulistuj@gmail.com>
2023-01-21 17:10:16 +0200

    Update jenkinsfile

--- testscript.sh
+++ testscript.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-TERRAFORM_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |  grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
 
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
+sudo apt update -y
 
-sudo apt install unzip -y
+sudo apt install apache2 php libapache2-mod-php -y
 
-unzip terraform_${TERRAFORM_VER}_linux_amd64.zip -y
+sudo mkdir -p /var/www/html/
 
-sudo mv terraform /usr/local/bin/
+sudo echo "<h1>Created with  Jenkins by Serhii Valchuk<h1>" > /var/www/html/index.html
 
-terraform version
+sudo service apache2 restart
+
+#TERRAFORM_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |  grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+
+#wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
+
+#sudo apt install unzip -y
+
+#unzip terraform_${TERRAFORM_VER}_linux_amd64.zip -y
+
+#sudo mv terraform /usr/local/bin/
+
+#terraform version
