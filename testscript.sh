f8dea9cd495ea12e56802cd5b46d31ff7c1e3cb8
serhiy-v <sergozhulistuj@gmail.com>
2023-01-21 14:39:10 +0200

    Updated script

--- testscript.sh
+++ testscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt update -y
+apt update -y
 
-sudo apt install apache2 php libapache2-mod-php -y
+apt install apache2 php libapache2-mod-php -y
 
-sudo mkdir -p /var/www/html/
+mkdir -p /var/www/html/
 
-sudo echo "<h1>Created with  Jenkins by Serhii Valchuk<h1>" > /var/www/html/index.html
+echo "<h1>Created with  Jenkins by Serhii Valchuk<h1>" > /var/www/html/index.html
 
-sudo service apache2 restart
+service apache2 restart
